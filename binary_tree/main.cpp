9a39cc0a10e8194b09ce7b5fab77960bd3259a2f
latdmc <l.a.t.d.m.c@gmail.com>
2017-04-18 21:08:03 -0700

    Initial Commit

---
+++ binary_tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  binary_tree
+//
+//  Created by 劉祐任 on 18/04/2017.
+//  Copyright © 2017 劉祐任. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
